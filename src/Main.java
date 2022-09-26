ce58bcbe811bd9c44a3e01147a7ee583be051236
TomassSimko <simko.t@email.cz>
2022-09-26 16:11:31 +0200

    initial

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+            
+    }
+}
